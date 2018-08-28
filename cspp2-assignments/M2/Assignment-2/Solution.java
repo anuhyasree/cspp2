7b019b81d25ac82a55bef44df0e09c8cf8006f98
anuhya45 <sreeanuhya45@msitprogram.net>
2018-08-28 20:46:53 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- cspp2-assignments/M2/Assignment-2/Solution.java
+++ cspp2-assignments/M2/Assignment-2/Solution.java
@@ -1,58 +1,49 @@
-
-/**
+/**/
+/**.
  * { item_description }
  */
-
 import java.util.Scanner;
-
-/**
+/**.
  * Class for solution.
  */
-
-final class Solution {
-    /**
+public final class Solution {
+    /**.
      * Constructs the object.
      */
     private Solution() {
-        /**
+        /**.
          * { item_description }
          */
     }
     /**.
      * { function_description }
      *
-     * @param      a     { parameter_description }
-     * @param      b     { parameter_description }
-     * @param      c     { parameter_description }
+     * @param      args  The arguments
      */
-    public static void rootQuad(final int a, final int b, final int  c) {
-        /**
-         * { item_description }
-         */
-        double root1, root2;
-        int y = b * b - 2 * 2 * a * c;
-             root1 = (-b + java.lang.Math.sqrt(y)) / (2 * a);
-             root2 = (-b - java.lang.Math.sqrt(y)) / (2 * a);
-            System.out.println(root1 + " " + root2);
-        }
-        /**.
-         * { function_description }
-         *
-         * @param      args  The arguments
-         */
     public static void main(final String[] args) {
-        /**
-         * { var_description }
-         */
-        Scanner scan = new Scanner(System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
-        int c = scan.nextInt();
-        rootQuad(a, b, c);
+        Scanner s = new Scanner(System.in);
+        int base = s.nextInt();
+        int exponent = s.nextInt();
+        long result = power(base, exponent);
+        System.out.println(result);
     }
-    /*
-    Need to write the rootsOfQuadraticEquation function and print the output.
-    */
+    /**.
+     * { function_description }
+     *
+     * @param      base      The base
+     * @param      exponent  The exponent
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public static long power(final int base, final int exponent)
+    /**.
+     * { item_description }
+     */ {
+        if (exponent != 0) {
+            return base * power(base, exponent - 1);
+        } else {
+            return 1;
+        }
+    }
 }
-
 
