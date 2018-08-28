76a7f98f094f8eb5ef5f71acbddc4986db34eafb
anuhya45 <sreeanuhya45@msitprogram.net>
2018-08-28 11:17:34 +0530

    cspp2

---
+++ cspp2-assignments/M1/Assignment-1/Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in)
+		String name;
+		int rollNumber;
+		name = sc.nextLine();
+		rollNumber = sc.nextInt();
+		System.out.println("Name :"+name\n+ " Roll Number : "+rollNumber);
+	}
+}
