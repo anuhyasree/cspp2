79e44c4cd6e97ecd944ab44284f6cd18e560ff90
anuhya45 <sreeanuhya45@msitprogram.net>
2018-08-29 15:13:39 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/M2/Assignment-3/Solution.java
+++ cspp2-assignments/M2/Assignment-3/Solution.java
@@ -1,27 +1,20 @@
- /**
- * { item_description }
+/**.
+ * { Program to find the a power b with the hep of recursion }
  */
 import java.util.Scanner;
-/**
+/**.
  * Class for solution.
  */
-public class Solution {
-    /**
-     * { function_description }
-     *
-     * @param      args  The arguments
+public final class Solution {
+    /**.
+     * Constructs the object.
      */
-    public static void main(final String[] args) {
-        Scanner s = new Scanner(System.in);
-        int base = s.nextInt();
-        int exponent = s.nextInt();
-        long result = power(base, exponent);
-        System.out.println(result);
+    private Solution() {
+        /**.
+         * { item_description }
+         */
     }
-    /*
-    Need to write the power function and print the output.
-    */
-    /**
+    /**.
      * { function_description }
      *
      * @param      base      The base
@@ -29,24 +22,32 @@ public class Solution {
      *
      * @return     { description_of_the_return_value }
      */
-    public static long power(int base, int exponent) {
+    public static long power(final int base, final int exponent) {
+        /**.
+         * {Recursive function to find the power }
+         */
         if (exponent != 0) {
-
             return (base * power(base, exponent - 1));
-        }
-         else {
+        } else {
             return 1;
         }
-        
     }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        /**.
+         * { Main Function}
+         */
+        Scanner s = new Scanner(System.in);
+        int base = s.nextInt();
+        int exponent = s.nextInt();
+        long result = power(base, exponent);
+        System.out.println(result);
+    }
+    /**.
+    Need to write the power function and print the output.
+    */
 }
-
-
-
-
-
-
-
-
-
-
