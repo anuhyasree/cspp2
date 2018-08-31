14b7175a013471ca7a653e528245993d781ec18e
anuhya45 <sreeanuhya45@msitprogram.net>
2018-08-31 12:32:52 +0530

    concatinate

---
+++ cspp2-practice/m4/Concatinate/Concatinate.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Concatinate{
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String word = scan.next();
+		System.out.println("Hello "+word+"!");
+	}
+}
