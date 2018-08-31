ca4f05d08373c405c527a4878ca4c359e2140083
anuhya45 <sreeanuhya45@msitprogram.net>
2018-08-31 15:07:05 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/M4/Assignment-4/Solution.java
+++ cspp2-assignments/M4/Assignment-4/Solution.java
@@ -3,13 +3,13 @@
 // value for the given binary string.
 import java.util.Scanner;
 /**.
- * 
- * 
+ *
+ *
  * Class for solution.
  */
-final class Solution
-{/*
-	Do not modify the main function 
+final class Solution {
+   /*
+	Do not modify the main function
 	*/
 	/**.
 	 * { item_description }
@@ -26,14 +26,14 @@ final class Solution
 	{
 		Scanner sc = new Scanner(System.in);
 		String s = sc.nextLine();
-		String reverse = reverseString(s);	
+		String reverse = reverseString(s);
 		System.out.println(reverse);
 		
 	}
 	public static String reverseString(String s){
 		String reverse = " ";
 		for (int i = 0; i < s.length(); i++) {
-			reverse = s.charAt(i) + reverse;		 	
+			reverse = s.charAt(i) + reverse;
 		 } 
 		 return reverse;
 	}
