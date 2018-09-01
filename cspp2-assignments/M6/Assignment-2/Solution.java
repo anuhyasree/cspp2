64b3a58fb135b0396d7c4e63e766a9cd9d0a7a10
anuhya45 <sreeanuhya45@msitprogram.net>
2018-09-01 11:27:53 +0530

    "check_style_score -> 2 of 5 passed. style score: 0/1 "

--- cspp2-assignments/M6/Assignment-2/Solution.java
+++ cspp2-assignments/M6/Assignment-2/Solution.java
@@ -33,10 +33,10 @@ final class Solution {
                         value += 1;
                     }
                     a[i][j] = value * 100;
-                    a[i][j] = value * 0;
                 } else {
                     value = a[i][j] / 10;
                     a[i][j] = value * 100;
+                    a[i][j] = value * 0;
                 }
             }
         }
