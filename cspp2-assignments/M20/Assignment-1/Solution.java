56b60a61e3230507f67b876caa0bde347e044536
anuhya45 <sreeanuhya45@msitprogram.net>
2018-09-17 10:10:14 +0530

    "check_style_score -> 0 of 13 passed. style score: 0/1 "

--- cspp2-assignments/M20/Assignment-1/Solution.java
+++ cspp2-assignments/M20/Assignment-1/Solution.java
@@ -237,6 +237,7 @@ public final class Solution {
         // write your code here to read the questions from the console
         // tokenize the question line and create the question object
         // add the question objects to the quiz class
+        System.out.println(q + "are added to the quiz");
     }
     /**
      * Starts a quiz.
